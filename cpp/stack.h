eb479f6ec2386a2487adc9e6406399c1107d8287
Callista Napitupulu <callistanapitupulu@unknown227f1efaba64.attlocal.net>
2024-11-21 20:49:24 -0800

    clean up

--- cpp/stack.h
+++ cpp/stack.h
@@ -7,73 +7,85 @@ using namespace std;
 #define INITIAL_CAPACITY 16
 
 template <typename T>
-class Stack {
+class Stack
+{
 
   unique_ptr<T[]> elements;
   int capacity;
   int top;
 
-  Stack(const Stack<T>&) = delete;
-  Stack<T>& operator=(const Stack<T>&) = delete;
-  
+  Stack(const Stack<T> &) = delete;
+  Stack<T> &operator=(const Stack<T> &) = delete;
+
 public:
-  Stack():
-    top(0),
-    capacity(INITIAL_CAPACITY),
-    elements(make_unique<T[]>(INITIAL_CAPACITY)) {
+  Stack() : top(0),
+            capacity(INITIAL_CAPACITY),
+            elements(make_unique<T[]>(INITIAL_CAPACITY))
+  {
+  }
 
-    }
-
-  int size() const {
+  int size() const
+  {
     return top;
   }
 
-  bool is_empty() const {
+  bool is_empty() const
+  {
     return top == 0;
   }
 
-  bool is_full() const {
+  bool is_full() const
+  {
     return top == MAX_CAPACITY;
   }
 
-  void push(T item) {
-      if (is_full()) {
-          throw overflow_error("Stack has reached maximum capacity");
-      }
-      if (top == capacity) {
-          reallocate(2 * capacity);
-      }
-      elements[top++] = item;
+  void push(T item)
+  {
+    if (is_full())
+    {
+      throw overflow_error("Stack has reached maximum capacity");
+    }
+    if (top == capacity)
+    {
+      reallocate(2 * capacity);
+    }
+    elements[top++] = item;
   }
 
-  T pop() {
-      if (is_empty()) {
-          throw underflow_error("cannot pop from empty stack");
-      }
+  T pop()
+  {
+    if (is_empty())
+    {
+      throw underflow_error("cannot pop from empty stack");
+    }
 
-      T popped_value = elements[--top];
-      elements[top] = T();
+    T popped_value = elements[--top];
+    elements[top] = T();
 
-      if (top <= capacity / 4 && capacity > INITIAL_CAPACITY) {
-          reallocate(max(capacity / 2, INITIAL_CAPACITY));
-      }
+    if (top <= capacity / 4 && capacity > INITIAL_CAPACITY)
+    {
+      reallocate(max(capacity / 2, INITIAL_CAPACITY));
+    }
 
-      return popped_value;
+    return popped_value;
   }
-  
+
 private:
-  void reallocate(int new_capacity) {
-      if (new_capacity > MAX_CAPACITY) {
-          new_capacity = MAX_CAPACITY;
-      }
-      if (new_capacity < INITIAL_CAPACITY) {
-          new_capacity = INITIAL_CAPACITY;
-      }
+  void reallocate(int new_capacity)
+  {
+    if (new_capacity > MAX_CAPACITY)
+    {
+      new_capacity = MAX_CAPACITY;
+    }
+    if (new_capacity < INITIAL_CAPACITY)
+    {
+      new_capacity = INITIAL_CAPACITY;
+    }
 
-      unique_ptr<T[]> new_elements = make_unique<T[]>(new_capacity);
-      copy(elements.get(), elements.get() + top, new_elements.get());
+    unique_ptr<T[]> new_elements = make_unique<T[]>(new_capacity);
+    copy(elements.get(), elements.get() + top, new_elements.get());
 
-      elements = std::move(new_elements);
-      capacity = new_capacity;
+    elements = std::move(new_elements);
+    capacity = new_capacity;
   }
 };
