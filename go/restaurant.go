e41824ebcaf0539148d8f09573eda170d35acf88
Callista Napitupulu <callistanapitupulu@Callistas-MacBook-Pro.local>
2024-12-04 18:08:59 -0800

    added notes from homework helper

--- go/restaurant.go
+++ go/restaurant.go
@@ -3,6 +3,8 @@ package main
 import (
 	"log"
 	"math/rand"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
@@ -12,10 +14,56 @@ import (
 // and sleep the current goroutine for that much time.
 
 func do(seconds int, action ...any) {
-    log.Println(action...)
-    randomMillis := 500 * seconds + rand.Intn(500 * seconds)
-    time.Sleep(time.Duration(randomMillis) * time.Millisecond)
+	log.Println(action...)
+	randomMillis := 500*seconds + rand.Intn(500*seconds)
+	time.Sleep(time.Duration(randomMillis) * time.Millisecond)
 }
 
-// Implement the rest of the simulation here. You may need to add more imports
-// above.
+type Order struct {
+	id       uint64
+	customer string
+	// make a replay which is a channel that can take the order (HINT you need a pointer to the order)
+	// Also the name of the cook
+}
+
+var nextId atomic.Uint64
+
+// A waiter can only hold 3 orders at once
+var Waiter = make(chan *Order, 3)
+
+func Cook(name string) {
+	// log that the cook is starting
+	// loop forever
+	//   wait for an order from the waiter
+	//   cook it
+	//   put the name of the cook in the order
+	//   send it back into the reply channel: order.replay <- order
+
+}
+
+func Customer(name string, wg *sync.WaitGroup) {
+	for mealsEaten := 0; mealsEaten < 5; {
+		// place an order
+		// select statement so that if the waiter gets it with 7 seconds
+		// then you get it from the cook, and eat it (mealsEaten++)
+		// If you don't get leave the restaurant
+		// do(5, name, "is waiting too long, abandoning the order, order.id)
+	}
+}
+
+func main() {
+	customers := [10]string{
+		"Ani", "Bai", "Cat", "Dao", "Eve", "Fay", "Gus", "Hua", "Iza", "Jai",
+	}
+	// In a loop start each customer as a goroutine
+	var wg sync.WaitGroup
+	for _, customer := range customers {
+		wg.Add(1)
+		go Customer(customer, &wg)
+	}
+	// Start 3 cooks Remy, Linguini, and Colette
+	go Cook("Remy")
+	go Cook("Linguini")
+	go Cook("Colette")
+	log.Println("Restaurant is closing")
+}
